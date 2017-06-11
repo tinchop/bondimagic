024448e96dc26e8add825b977d70958a3d6373ce
tinchop <martin@magnetico.com.ar>
2017-06-11 15:49:54 -0300

    geolib added to project

--- bondimagic-server/src/server.js
+++ bondimagic-server/src/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var geolib = require('geolib');
 
 mongoose.connect('mongodb://localhost:27017/bondimagic');
 
@@ -44,28 +45,15 @@ app.listen(3000, function () {
 
 
 app.get('/distance_test', function(req, res){
+
  var lat1 = '-34.6279826';//-34.6279826,-58.433789
  var lon1 = '-58.433789';
  var lat2 = '-34.6325845';//-34.6325845,-58.4576792
  var lon2 = '-58.4576792';
- var distancia = getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2);
- res.send({ distancia: distancia});
+
+ var locationA = {latitude: lat1, longitude: lon1};
+ var locationB = {latitude: lat2, longitude: lon2};
+ var distancia = {distancia: geolib.getDistance(locationA, locationB)};
+ res.send(distancia);
 });
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
