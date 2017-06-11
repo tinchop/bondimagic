ce7efa6b03a8fcda7c46258117bffda0a5bdd394
martin <martin@magnetico.com.ar>
2017-06-11 14:55:27 -0300

    distance function

--- bondimagic-server/server.js
+++ bondimagic-server/server.js
@@ -41,3 +41,31 @@ app.get('/ramales', function(req, res){
 app.listen(3000, function () {
   console.log('Bondimagic server listening on port 3000!');
 });
+
+
+app.get('/distance_test', function(req, res){
+ var lat1 = '-34.6279826';//-34.6279826,-58.433789
+ var lon1 = '-58.433789';
+ var lat2 = '-34.6325845';//-34.6325845,-58.4576792
+ var lon2 = '-58.4576792';
+ var distancia = getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2);
+ res.send({ distancia: distancia});
+});
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
