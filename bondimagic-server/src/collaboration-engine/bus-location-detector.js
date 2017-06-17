fa94ba41597489c4fee46070c1c5ac94334bf8e1
swandelow <swandelow@despegar.com>
2017-06-17 18:24:22 -0300

    agrupar por ramal en busLocationDetector

--- bondimagic-server/src/collaboration-engine/bus-location-detector.js
+++ bondimagic-server/src/collaboration-engine/bus-location-detector.js
@@ -1,13 +1,31 @@
 const DEFAULT_UMBRAL = 2;
 
+
 class BusLocationDetector {
     constructor() {
         this.umbral = DEFAULT_UMBRAL;
     }
 
     detectLocations(userLocations) {
+        let groupByBusRoute = this._groupByBusRoute(userLocations);
         let detectedBus = [];
         return detectedBus;
+    }
+
+    _groupByBusRoute(userLocations) {
+        let result = new Map();
+        userLocations.forEach((userLocation) => {
+            let busRouteId = userLocation.busRouteId;
+            let users = result.get(userLocation.busRouteId);
+            if (users) {
+                users.push(userLocation);
+                result.set(busRouteId, users);
+            } else {
+                result.set(busRouteId, []);
+            }
+        });
+
+        return result;
     }
 }
 
