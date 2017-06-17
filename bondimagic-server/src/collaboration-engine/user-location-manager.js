83b104b454079cb89e20752db347bcb6b81fbe7d
tinchop <martin@magnetico.com.ar>
2017-06-17 17:14:06 -0300

    Minor fixes

--- bondimagic-server/src/collaboration-engine/user-location-manager.js
+++ bondimagic-server/src/collaboration-engine/user-location-manager.js
@@ -10,18 +10,31 @@ class UserLocationManager {
     process(user, location, busRouteId) {
         let userLocation = this.userLocations.get(user);
         if (userLocation) {
-            this._updateUserLocation(userLocation, location);
+            this._updateUserLocation(userLocation, location, busRouteId);
         } else {
-            console.log("agrego nuevo user location.");
+            console.log("Agrego nuevo UserLocation para ", user);
             this.userLocations.set(user, new UserLocation(user, busRouteId, location));
         }
     }
 
-    _updateUserLocation(userLocation, newLocation) {
-        console.log("ahora tengo que hacer update de user location");
-        userLocation.update(newLocation);
+    _updateUserLocation(userLocation, newLocation, busRouteId) {
+        userLocation.update(newLocation, this._locationWithinBusRoute(newLocation, busRouteId));
     }
 
+    _locationWithinBusRoute(location, busRouteId) {
+        //desde paseo colon y eeuu hasta paseo colon y alsina
+        let polygon = [
+            { latitude: -34.6183359, longitude: -58.3687782 },
+            { latitude: -34.6183006, longitude: -58.3693522 },
+            { latitude: -34.6100032, longitude: -58.369414 },
+            { latitude: -34.6100087, longitude: -58.3695783 }
+        ];
+        let segment = new RouteSegment(polygon);
+        let busRoute = new BusRoute();
+        busRoute.addSegment(segment);
+
+        return busRoute.contains(location);
+    }
 }
 
 module.exports = UserLocationManager;
--- bondimagic-server/src/collaboration-engine/user-location.js
+++ bondimagic-server/src/collaboration-engine/user-location.js
@@ -17,6 +17,7 @@ class UserLocation {
         this.travelledDistance += this._calculateDistance(this.previousLocation, newLocation);
         this.lastRequestTimestamp = new Date();
         this.inRoute = inRoute;
+        console.log(this.userId, ": distancia recorrida=", this.travelledDistance, " en ruta de ", this.busRouteId, ": ", (this.inRoute ? "Sí" : "No"));
     }
 
     _calculateDistance(prevLocation, newLocation) {
