a769d9edf1626ab7f24503ee35b06c63796c61f2
tinchop <martin@magnetico.com.ar>
2017-06-17 14:56:09 -0300

    Express body-parser

--- bondimagic-server/src/server.js
+++ bondimagic-server/src/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 var geolib = require('geolib');
+var bodyParser = require("body-parser");
 const BondiMagicService = require('./bondi-magic-service');
 
 let bondiMagicService = new BondiMagicService();
 
-app.post('/nearbybuses', function(req, res){
+app.use(bodyParser.json());
+
+app.post('/nearbybuses', function (req, res) {
+	console.log(req.body);
 	var user = req.body['user'];
 	var location = req.body['location'];
 	var busRouteId = req.body['busRouteId'];
@@ -14,9 +18,9 @@ app.post('/nearbybuses', function(req, res){
 });
 
 app.get('/', function (req, res) {
-  res.send('This is the Bondimagic server, pal.');
+	res.send('This is the Bondimagic server, pal.');
 });
 
 app.listen(3000, function () {
-  console.log('Bondimagic server listening on port 3000!');
+	console.log('Bondimagic server listening on port 3000!');
 });
--- dummy-client/client.js
+++ dummy-client/client.js
@@ -4,21 +4,21 @@ setInterval(juanRequest, 2000);
 
 function juanRequest() {
 	var requestBody = {
-			user: 'Juan',
-			busRouteId: 10,
-			location: {
-				latitude: 12,
-				longitude: 12
-			}
-		};
+		user: 'Juan',
+		busRouteId: 10,
+		location: {
+			latitude: 12,
+			longitude: 12
+		}
+	};
 	var options = {
-		url: 'http://localhost:3000',
+		url: 'http://localhost:3000/nearbybuses',
 		method: 'POST',
 		json: true,
-		body: JSON.stringify(requestBody)
+		body: requestBody
 	};
 
 	request(options, function (error, response, body) {
-		console.log(response.statusCode);
+		console.log(body);
 	});
 }
