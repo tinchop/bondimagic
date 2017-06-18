4569a446e9139e4d40af1a240dd0097329cd6670
swandelow <swandelow@despegar.com>
2017-06-18 00:28:39 -0300

    primer commit de la app mobile en ionic

---
+++ bondimagic-app/src/pages/map/map.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html'
+})
+export class MapPage {
+
+}
