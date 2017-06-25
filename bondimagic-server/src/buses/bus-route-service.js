65439005f8a0019bfe14d6598af11b2b6e911bfb
swandelow <swandelow@despegar.com>
2017-06-24 22:12:52 -0300

    agrego recorrido del 8

--- bondimagic-server/src/buses/bus-route-service.js
+++ bondimagic-server/src/buses/bus-route-service.js
@@ -20,10 +20,50 @@ class BusRouteService {
 
         this.routes = new Map();
         this.routes.set(59, busRoute);
+
+        this.routes.set(8, this._buildBusRoute8());
     }
 
     getRoute(busRouteId) {
         return this.routes.get(busRouteId);
+    }
+
+    _buildBusRoute8 () {
+        // Desde Rivadavia y Cerrito, Hasta Rivadavia y Paraná
+        let polygon1 = [
+            { latitude: -34.60838053, longitude: -58.38200748 },
+            { latitude: -34.60847104, longitude: -58.38200882 },
+            { latitude: -34.60892139, longitude: -58.3875154 },
+            { latitude: -34.60884412, longitude: -58.38754222 }
+        ];
+        // desde Rivadavia y Paraná, Hasta Rivadavia y av. Entre Rios
+        let polygon2 = [
+            { latitude: -34.60885737, longitude: -58.3876951 },
+            { latitude: -34.60893463, longitude: -58.38766292 },
+            { latitude: -34.60929889, longitude: -58.39188471 },
+            { latitude: -34.60923707, longitude: -58.39189745 }
+        ];
+        // desde Rivadavia y av. Entre Rios, Hasta Rivadavia y Callao (pequeña curva)
+        let polygon3 = [
+            { latitude: -34.60927791, longitude: -58.39191791 },
+            { latitude: -34.60928012, longitude: -58.3919521 },
+            { latitude: -34.60914656, longitude: -58.39196686 },
+            { latitude: -34.60914436, longitude: -58.39192864 }
+        ];
+        // desde Rivadavia y Callao, Hasta Rivadavia y av. Boedo
+        let polygon4 = [
+            { latitude: -34.60913553, longitude: -58.39199536 },
+            { latitude: -34.60919403, longitude: -58.39198597 },
+            { latitude: -34.61104285, longitude: -58.41840908 },
+            { latitude: -34.61095897, longitude: -58.4184359 }
+        ];
+
+        let busRoute = new BusRoute();
+        for (var polygon in [polygon1, polygon2, polygon3, polygon4]) {
+            let segment = new RouteSegment(polygon);
+            busRoute.addSegment(segment);
+        }
+        return busRoute;
     }
 
 }
